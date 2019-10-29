613c9266dad721a12084fdc2eb1bdf73488cd08e
Jeremie Dumas <jeremie.dumas@ens-lyon.org>
2019-10-29 16:56:35 -0400

    Update FindMKL + options.

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -13,7 +13,7 @@
 #  MKL_FOUND - System has MKL
 #  MKL_INCLUDE_DIRS - MKL include files directories
 #  MKL_LIBRARIES - The MKL libraries
-#  MKL_COMPILE_OPTIONS - Compilation flags to use with MKL
+#  MKL_COMPILE_DEFINITIONS - Compilation flags to use with MKL
 #  MKL_INTERFACE_LIBRARY - MKL interface library
 #  MKL_SEQUENTIAL_LAYER_LIBRARY - MKL sequential layer library
 #  MKL_CORE_LIBRARY - MKL core library
@@ -21,6 +21,9 @@
 #  The environment variables MKLROOT and INTEL are used to find the library.
 #  Everything else is ignored. If MKL is found "-DMKL_ILP64" is added to
 #  CMAKE_C_FLAGS and CMAKE_CXX_FLAGS.
+#
+#  If the variable MKL_USE_ILP64 is set to ON, links against the ILP64 interface,
+#  and add -DMKL_ILP64 to the compile definitions.
 #
 #  Example usage:
 #
@@ -31,45 +34,61 @@
 
 # If already in cache, be silent
 if(MKL_INCLUDE_DIRS
-   AND MKL_LIBRARIES
-   AND MKL_INTERFACE_LIBRARY
-   AND MKL_SEQUENTIAL_LAYER_LIBRARY
-   AND MKL_CORE_LIBRARY)
-  set(MKL_FIND_QUIETLY TRUE)
+        AND MKL_LIBRARIES
+        AND MKL_INTERFACE_LIBRARY
+        AND MKL_SEQUENTIAL_LAYER_LIBRARY
+        AND MKL_CORE_LIBRARY)
+    set(MKL_FIND_QUIETLY TRUE)
 endif()
 
 if(NOT MKL_SHARED_LIBS)
-  if(WIN32)
-      if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-          set(INT_LIB "mkl_intel_lp64.lib")
-      else()
-          set(INT_LIB "mkl_intel_c.lib")
-      endif()
-      set(SEQ_LIB "mkl_sequential.lib")
-      set(THR_LIB "mkl_intel_thread.lib")
-      set(COR_LIB "mkl_core.lib")
-  else()
-      set(INT_LIB "libmkl_intel_lp64.a")
-      set(SEQ_LIB "libmkl_sequential.a")
-      set(THR_LIB "libmkl_intel_thread.a")
-      set(COR_LIB "libmkl_core.a")
-  endif()
-else()
-  if(WIN32)
-    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-      set(INT_LIB "mkl_intel_lp64_dll.lib")
+    if(WIN32)
+        if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+            if(MKL_USE_ILP64)
+                set(INT_LIB "mkl_intel_ilp64.lib")
+            else()
+                set(INT_LIB "mkl_intel_lp64.lib")
+            endif()
+        else()
+            set(INT_LIB "mkl_intel_c.lib")
+        endif()
+        set(SEQ_LIB "mkl_sequential.lib")
+        set(THR_LIB "mkl_intel_thread.lib")
+        set(COR_LIB "mkl_core.lib")
     else()
-      set(INT_LIB "mkl_intel_c_dll.lib")
+        if(MKL_USE_ILP64)
+            set(INT_LIB "libmkl_intel_ilp64.a")
+        else()
+            set(INT_LIB "libmkl_intel_lp64.a")
+        endif()
+        set(SEQ_LIB "libmkl_sequential.a")
+        set(THR_LIB "libmkl_intel_thread.a")
+        set(COR_LIB "libmkl_core.a")
     endif()
-    set(SEQ_LIB "mkl_sequential_dll.lib")
-    set(THR_LIB "mkl_intel_thread_dll.lib")
-    set(COR_LIB "mkl_core_dll.lib")
-  else()
-    set(INT_LIB "libmkl_intel_lp64.so")
-    set(SEQ_LIB "libmkl_sequential.so")
-    set(THR_LIB "libmkl_intel_thread.so")
-    set(COR_LIB "libmkl_core.so")
-  endif()
+else()
+    if(WIN32)
+        if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+            if(MKL_USE_ILP64)
+                set(INT_LIB "mkl_intel_ilp64_dll.lib")
+            else()
+                set(INT_LIB "mkl_intel_lp64_dll.lib")
+            endif()
+        else()
+            set(INT_LIB "mkl_intel_c_dll.lib")
+        endif()
+        set(SEQ_LIB "mkl_sequential_dll.lib")
+        set(THR_LIB "mkl_intel_thread_dll.lib")
+        set(COR_LIB "mkl_core_dll.lib")
+    else()
+        if(MKL_USE_ILP64)
+            set(INT_LIB "libmkl_intel_ilp64.so")
+        else()
+            set(INT_LIB "libmkl_intel_lp64.so")
+        endif()
+        set(SEQ_LIB "libmkl_sequential.so")
+        set(THR_LIB "libmkl_intel_thread.so")
+        set(COR_LIB "libmkl_core.so")
+    endif()
 endif()
 
 find_path(MKL_INCLUDE_DIR NAMES mkl.h HINTS $ENV{MKLROOT}/include)
@@ -99,28 +118,22 @@ set(MKL_INCLUDE_DIRS ${MKL_INCLUDE_DIR})
 set(MKL_LIBRARIES ${MKL_INTERFACE_LIBRARY} ${MKL_SEQUENTIAL_LAYER_LIBRARY} ${MKL_CORE_LIBRARY})
 
 if(MKL_INCLUDE_DIR
-   AND MKL_INTERFACE_LIBRARY
-   AND MKL_SEQUENTIAL_LAYER_LIBRARY
-   AND MKL_CORE_LIBRARY)
+        AND MKL_INTERFACE_LIBRARY
+        AND MKL_SEQUENTIAL_LAYER_LIBRARY
+        AND MKL_CORE_LIBRARY)
 
-  if(NOT DEFINED ENV{CRAY_PRGENVPGI}
-     AND NOT DEFINED ENV{CRAY_PRGENVGNU}
-     AND NOT DEFINED ENV{CRAY_PRGENVCRAY}
-     AND NOT DEFINED ENV{CRAY_PRGENVINTEL})
-    set(ABI "-m64")
-  endif()
-
-  set(MKL_COMPILE_OPTIONS "-DMKL_ILP64 ${ABI}")
-
+    if(MKL_USE_ILP64)
+        set(MKL_COMPILE_DEFINITIONS "-DMKL_ILP64")
+    else()
+        set(MKL_COMPILE_DEFINITIONS "")
+    endif()
 else()
-
-  set(MKL_INCLUDE_DIRS "")
-  set(MKL_LIBRARIES "")
-  set(MKL_COMPILE_OPTIONS "")
-  set(MKL_INTERFACE_LIBRARY "")
-  set(MKL_SEQUENTIAL_LAYER_LIBRARY "")
-  set(MKL_CORE_LIBRARY "")
-
+    set(MKL_INCLUDE_DIRS "")
+    set(MKL_LIBRARIES "")
+    set(MKL_COMPILE_DEFINITIONS "")
+    set(MKL_INTERFACE_LIBRARY "")
+    set(MKL_SEQUENTIAL_LAYER_LIBRARY "")
+    set(MKL_CORE_LIBRARY "")
 endif()
 
 # Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if all
@@ -129,7 +142,6 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MKL
                                   DEFAULT_MSG
                                   MKL_LIBRARIES
-                                  MKL_COMPILE_OPTIONS
                                   MKL_INCLUDE_DIRS
                                   MKL_INTERFACE_LIBRARY
                                   MKL_SEQUENTIAL_LAYER_LIBRARY
@@ -137,7 +149,18 @@ find_package_handle_standard_args(MKL
 
 mark_as_advanced(MKL_INCLUDE_DIRS
                  MKL_LIBRARIES
-                 MKL_COMPILE_OPTIONS
+                 MKL_COMPILE_DEFINITIONS
                  MKL_INTERFACE_LIBRARY
                  MKL_SEQUENTIAL_LAYER_LIBRARY
                  MKL_CORE_LIBRARY)
+
+if(NOT TARGET mkl::mkl)
+    add_library(mkl::mkl UNKNOWN IMPORTED GLOBAL)
+    set_target_properties(mkl::mkl PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${MKL_INCLUDE_DIRS}"
+        IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
+        IMPORTED_LOCATION "${MKL_CORE_LIBRARY}"
+        IMPORTED_LINK_INTERFACE_LIBRARIES "${MKL_INTERFACE_LIBRARY};${MKL_SEQUENTIAL_LAYER_LIBRARY}"
+        INTERFACE_COMPILE_DEFINITIONS "${MKL_COMPILE_DEFINITIONS}"
+    )
+endif()
